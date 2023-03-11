823516bf1f7aafede947d873cabea1b904dd0439
ed4oz <dyt.psk.edaozyurt@gmail.com>
2023-03-11 22:09:03 +0300

    homework completed

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Homework homework=new Homework();
+        homework.guessNumber();
+        homework.wordReverse();
+
+    }
+}
